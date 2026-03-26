8b729b6fddf112558570f91a35f5a3f2cb935821
mikeli94cn <lishuo94@126.com>
2026-03-27 05:39:04 +0800

    condition_loop

---
+++ condition_loop.cpp
@@ -0,0 +1,17 @@
+#include <format>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string today = "Monday";
+  string mood;
+  if (today == "Friday") {
+    mood = "happy";
+  } else {
+    mood = "sad";
+  }
+  string output = format("today is {},mood is {}\n", today, mood);
+  cout << output;
+}
